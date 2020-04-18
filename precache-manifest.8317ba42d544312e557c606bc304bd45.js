350b3573b5b7ceafb9e5277206f0d866ed86e66b
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2020-04-18 09:49:52 +0530

    Updates

---
+++ precache-manifest.8317ba42d544312e557c606bc304bd45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0fd1f45a6869536de7d8bd04d7368f6",
+    "url": "/Minesweeper/index.html"
+  },
+  {
+    "revision": "88bf347aedb6989437cd",
+    "url": "/Minesweeper/static/css/main.eaabb594.chunk.css"
+  },
+  {
+    "revision": "b6f1fb4a1efaaec16fb2",
+    "url": "/Minesweeper/static/js/2.1d11e59d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Minesweeper/static/js/2.1d11e59d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88bf347aedb6989437cd",
+    "url": "/Minesweeper/static/js/main.6abc43fb.chunk.js"
+  },
+  {
+    "revision": "4e012856e9f0f294b3c3",
+    "url": "/Minesweeper/static/js/runtime-main.8ba5e94c.js"
+  }
+]);
